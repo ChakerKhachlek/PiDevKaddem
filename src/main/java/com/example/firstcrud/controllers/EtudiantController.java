92e153b0cf86367636cbd57b0419fcf00641a52a
ChakerKhachlek <chaker-@hotmail.com>
2022-11-12 14:53:22 +0100

    added swagger api documentation

--- src/main/java/com/example/firstcrud/controllers/EtudiantController.java
+++ src/main/java/com/example/firstcrud/controllers/EtudiantController.java
@@ -3,33 +3,41 @@ package com.example.firstcrud.controllers;
 import com.example.firstcrud.entities.Contrat;
 import com.example.firstcrud.entities.Etudiant;
 import com.example.firstcrud.services.IEtudiantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Etudiant Management")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
 public class EtudiantController {
     IEtudiantService etudiantService;
 
+    @Operation(description = "Retrieve all etudiants")
     @GetMapping("/retrieve-all-etudiants")
     public List<Etudiant> getEtudiants() {
         List<Etudiant> listEquipes = etudiantService.retreiveAllEtudiants();
         return listEquipes;
     }
+
+    @Operation(description = "Add contracts to etudiant")
     @PostMapping("/add-contrats/{id-etudiant}")
     public void ajouterEtaffecterListeboutiques(@RequestBody List<Contrat> lc, @PathVariable("id-etudiant") Integer idEtudiant){
         etudiantService.addEtudiantContrats(lc, idEtudiant);
     }
+
+    @Operation(description = "Retreive etudiant by ID")
     @GetMapping("/retrieve-etudiant/{etudiant-id}")
     public Optional<Etudiant> retrieveEquipe(@PathVariable("etudiant-id") Integer etudiantId) {
         return etudiantService.retrieveEtudiant(etudiantId);
     }
 
-
+    @Operation(description = "Add etudiant")
     @PostMapping("/add-etudiant")
     public Etudiant addEtudiant(@RequestBody  Etudiant e) {
 
@@ -37,14 +45,18 @@ public class EtudiantController {
         return etud;
     }
 
-    @DeleteMapping("/remove-etudiant/{etudiant-id}")
-    public void removeEtudiant(@PathVariable("etudiant-id") Integer etudiantID) {
-        etudiantService.removeEtudiant(etudiantID);
-    }
 
+    @Operation(description = "Update etudiant")
     @PutMapping("/update-etudiant")
     public Etudiant updateEtudiant(@RequestBody Etudiant e) {
         Etudiant equipe= etudiantService.updateEtudiant(e);
         return equipe;
     }
+    
+    @Operation(description = "Remove etudiant")
+    @DeleteMapping("/remove-etudiant/{etudiant-id}")
+    public void removeEtudiant(@PathVariable("etudiant-id") Integer etudiantID) {
+        etudiantService.removeEtudiant(etudiantID);
+    }
+
 }
